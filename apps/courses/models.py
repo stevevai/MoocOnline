7356d342d7305c489f019fe1275e7d9d57c1260a
cloudnzk <526416486@qq.com>
2019-05-17 23:38:39 +0800

    Define course and teacher model.

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -1,31 +1,45 @@
-'''
 from django.db import models
 from datetime import datetime
 
-from organization.models import Teacher
+from teachers.models import Teacher
 
 from DjangoUeditor.models import UEditorField
+
+
+# 一级分类
+class CourseClassify(models.Model):
+    name = models.CharField(max_length=20, verbose_name=u"一级分类名称")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
+
+
+# 二级分类
+class CourseClassify2(models.Model):
+    name = models.CharField(max_length=20, verbose_name=u"二级分类名称")
+    parent_classify = models.ForeignKey(CourseClassify, verbose_name=u"一级分类", on_delete=models.SET_NULL)
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
 
 
 # Create your models here.
 class Course(models.Model):
     name = models.CharField(max_length=50, verbose_name=u"课程名")
-    desc = models.CharField(max_length=500, verbose_name=u"课程描述")
-    # 使用富文本编辑器
+    desc = models.CharField(max_length=500, verbose_name=u"课程简要描述")
+    # 富文本编辑器
     detail = UEditorField(verbose_name=u"课程详情",width=600, height=300, imagePath="courses/ueditor/", filePath="courses/ueditor/", default='')
     degree = models.CharField(choices=(("cj", "初级"), ("zj", "中级"), ("gj", "高级")), max_length=2, verbose_name=u"难度")
-    learn_times = models.IntegerField(default=0, verbose_name=u"学习时长(分钟")
+    learn_times = models.IntegerField(default=0, verbose_name=u"学习时长(分钟)")
     teacher = models.ForeignKey(Teacher, verbose_name=u"讲师", null=True, blank=True, on_delete=models.CASCADE)
-    students = models.IntegerField(default=0, verbose_name=u"学习人数")
+    stu_nums = models.IntegerField(default=0, verbose_name=u"学习人数")
     fav_nums = models.IntegerField(default=0, verbose_name=u"收藏人数")
     image = models.ImageField(upload_to="courses/%Y/%m", verbose_name="封面图", max_length=100)
-    click_nums = models.IntegerField(default=0, verbose_name=u"点击数")
-    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
-    category = models.CharField(max_length=20, default=u"", verbose_name=u"课程类别")
-    tag = models.CharField(max_length=15, verbose_name=u"课程标签", default=u"")
+    classify_root = models.ForeignKey(CourseClassify, verbose_name=u"一级分类", on_delete=models.SET_NULL, default="")
+    classify_detail = models.ForeignKey(CourseClassify, verbose_name=u"二级分类", on_delete=models.SET_NULL, default="")
+    # category = models.CharField(max_length=20, default=u"", verbose_name=u"课程类别")
+    # 加一张课程与标签的中间表
+    # tag = models.CharField(max_length=15, verbose_name=u"课程标签", default=u"")
     you_need_know = models.CharField(max_length=300, default=u"一颗勤学的心是本课程必要前提", verbose_name=u"课程须知")
     teacher_tell = models.CharField(max_length=300, default=u"按时交作业,不然叫家长", verbose_name=u"老师告诉你")
     is_banner = models.BooleanField(default=False, verbose_name=u"是否轮播")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
 
     class Meta:
         verbose_name = u"课程"
@@ -34,6 +48,7 @@ class Course(models.Model):
     def get_zj_nums(self):
         # 获取课程章节数的方法
         return self.lesson_set.all().count()
+
     # 自定义函数作为列
     get_zj_nums.short_description = "章节数"
 
@@ -45,6 +60,7 @@ class Course(models.Model):
         return self.name
 
 
+# 章
 class Lesson(models.Model):
     course = models.ForeignKey(Course, verbose_name=u"课程", on_delete=models.CASCADE)
     name = models.CharField(max_length=100, verbose_name=u"章节名")
@@ -62,9 +78,11 @@ class Lesson(models.Model):
         return self.video_set.all()
 
 
+# 节
 class Video(models.Model):
     lesson = models.ForeignKey(Lesson, verbose_name=u"章节", on_delete=models.CASCADE)
     name = models.CharField(max_length=100, verbose_name=u"视频名")
+    # 后期添加教师上传视频的入口
     url = models.CharField(max_length=200, default="", verbose_name=u"访问地址")
     add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
     # 使用分钟做后台记录(存储最小单位)前台转换
@@ -80,7 +98,7 @@ class Video(models.Model):
 
 class CourseResources(models.Model):
     course = models.ForeignKey(Course, verbose_name=u"课程", on_delete=models.CASCADE)
-    name = models.CharField(max_length=100, verbose_name=u"名称")
+    name = models.CharField(max_length=100, verbose_name=u"资源名称")
     download = models.FileField(upload_to="course/resource/%Y/%m", verbose_name=u"资源文件", max_length=100)
     add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
 
@@ -95,7 +113,4 @@ class BannerCourse(Course):
         verbose_name = "轮播课程"
         verbose_name_plural = verbose_name
         # 设置proxy = true会具有model的功能，但不会生成表
-'''
-'''
         proxy = True
-'''
--- apps/teachers/models.py
+++ apps/teachers/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
+import datetime
 # Create your models here.
+
+
+class Teacher(models.Model):
+    job_list = (
+        ("hd", u"后端开发工程师"), ("qd", u"前端开发工程师"),
+        ("yd", u"移动开发工程师"), ("test", u"软件测试工程师"),
+        ("linux", u"Linux系统工程师"), ("qz", u"全栈开发工程师"),
+        ("sf", u"算法工程师"), ("student", u"学生")
+    )
+    name = models.CharField(max_length=50, verbose_name=u"教师名")
+    image = models.ImageField(
+        default='image/default_t.png',
+        upload_to="teacher/images",
+        verbose_name=u"头像",
+        max_length=100)
+    gender = models.CharField(max_length=10, choices=(("male", u"男"), ("female", u"女"), ("secret", u"保密")),
+                              default="secret", verbose_name=u"性别")
+    mobile = models.CharField(max_length=11, null=True, blank=True)  # blank = True 代表表单非必填，null = True 属于数据库范畴
+    job = models.CharField(max_length=50, choices=job_list, verbose_name=u"公司职位")
+    points = models.CharField(max_length=50, verbose_name=u"教学特点", null=True)
+    fav_nums = models.IntegerField(default=0, verbose_name=u"收藏数")
+    add_time = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        verbose_name = u"教师"
+        verbose_name_plural = verbose_name
+
+    # 统计该教师的课程数量
+    def get_course_nums(self):
+        return self.course_set.all().count()
