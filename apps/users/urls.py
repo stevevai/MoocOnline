00a052ffb06e746171812539031a66689a43b62f
cloudnzk <526416486@qq.com>
2019-05-22 00:33:33 +0800

    个人中心。修改密码

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -3,7 +3,7 @@
 
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import UserInfoView, UploadImageView
+from .views import UserInfoView, UploadImageView, UpdatePwdView
 
 
 app_name = "users"
@@ -13,4 +13,6 @@ urlpatterns = [
     path('info/', UserInfoView.as_view(), name="user_info"),
     # 用户头像上传
     path('image/upload/', UploadImageView.as_view(), name="image_upload"),
+    # 修改密码
+    path('update/pwd/', UpdatePwdView.as_view(), name="update_pwd")
 ]
